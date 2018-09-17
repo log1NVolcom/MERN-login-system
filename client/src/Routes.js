13a115a99daa2fe5a44e4daa87d8f9a40e619cac
log1NVolcom <joaorego_5@hotmai.com>
2018-09-17 18:53:51 +0100

    Regist stop and warns about duplicate users

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -7,9 +7,7 @@ import RegisterPage from './components/Register/RegisterPage';
 import DashboardPage from './components/Dashboard/DashboardPage';
 import EditProfilePage from './components/EditProfile/EditProfilePage';
 
-
 const loggedIn = localStorage.getItem('userToken') !== null;
-const enterLogin = localStorage.getItem('userToken') === null;
 const Routes = () => (
   <Router>
     <Switch>
@@ -18,21 +16,26 @@ const Routes = () => (
         exact
         path="/Login"
         render={() =>
-          enterLogin ? <LoginPage /> : <Redirect to="/Dashboard" />
+          !loggedIn ? <LoginPage /> : <Redirect to="/Dashboard" />
         }
       />
-      <Route exact path="/Register" component={RegisterPage} />
+      <Route
+        exact
+        path="/Register"
+        render={() =>
+          !loggedIn ? <RegisterPage /> : <Redirect to="/Dashboard" />
+        }
+      />
       <Route
         exact
         path="/Dashboard"
         render={() => (loggedIn ? <DashboardPage /> : <Redirect to="/" />)}
-    />
- <Route
+      />
+      <Route
         exact
         path="/EditProfile"
         render={() => (loggedIn ? <EditProfilePage /> : <Redirect to="/" />)}
-    />
-
+      />
     </Switch>
   </Router>
 );
